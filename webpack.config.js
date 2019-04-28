29ac8eae38ab35d5bc89fdd0aa7ec85e3dee2b0f
bjlagman12 <bjlagman@gmail.com>
2019-04-28 16:55:13 -0700

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,13 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
-}
+};
